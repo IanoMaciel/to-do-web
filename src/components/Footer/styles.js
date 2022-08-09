7bbba000b11b0f04fc755bace39171b3dcf53c98
IanoMaciel <ianomaciel6385@gmail.com>
2022-08-08 20:18:01 -0400

    Ok Footer component

---
+++ src/components/Footer/styles.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    width: 100%;
+    height: 50px;
+    background: #20295F;
+    border-top: 5px solid #EE6B26;
+
+    position: absolute;
+    bottom: 0;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-family: 'Roboto', sans-serif;
+    font-weight: bold;
+    span {
+        color: #fff;
+    }
+`
