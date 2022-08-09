5c011029d8cc4e52f4b9422933d4d5d1170275a9
IanoMaciel <ianomaciel6385@gmail.com>
2022-08-09 10:28:09 -0400

    Modified

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -1,10 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
+import * as S from './styles';
+
+// componentes
+import Header from '../../components/Header';
+import Footer from '../../components/Footer';
+import FilterCard from '../../components/FilterCard';
 
 function Home() {
+  const [filterActivated, setFilterActivated] = useState();
   return (
-    <h1>
-      Página Home
-    </h1>
+    <S.Container>
+      <Header/>
+        <S.FilterArea>
+          <button type="button" onClick={() => setFilterActivated("all")}>
+            <FilterCard title="Todos" activated={filterActivated == 'all'}/>
+          </button>
+          <button type="button" onClick={() => setFilterActivated("today")}>
+            <FilterCard title="Hoje" activated={filterActivated == 'today'}/>
+          </button>
+          <button type="button" onClick={() => setFilterActivated("week")}>
+            <FilterCard title="Semana" activated={filterActivated == 'week'}/>
+          </button>
+          <button type="button" onClick={() => setFilterActivated("month")}>
+            <FilterCard title="Mes" activated={filterActivated == 'month'}/>
+          </button>
+          <button type="button" onClick={() => setFilterActivated("year")}>
+            <FilterCard title="Ano" activated={filterActivated == 'year'}/>
+          </button>
+          
+        </S.FilterArea>
+      <Footer/>
+    </S.Container>
   );
 }
 
---
+++ src/views/Home/styles.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    width: 100%;
+`
+
+export const FilterArea = styled.div`
+    width: 100%;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-around;
+    margin-top: 30px;
+
+    button {
+        border: none;
+        background: none;
+    }
+`
