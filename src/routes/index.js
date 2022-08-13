672c463fd8d9b8fa388b7829d03bfa1af2ff3c0d
IanoMaciel <ianomaciel6385@gmail.com>
2022-08-13 01:06:21 -0400

    creating routes for screen navigation

---
+++ src/routes/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import {
+    BrowserRouter,
+    Switch,
+    Route    
+} from 'react-router-dom';
+
+// import views
+import Home from '../views/Home';
+import Task from '../views/Task';
+
+
+export default function Routes() {
+    return(
+        <BrowserRouter>
+            <Switch>
+                <Route path="/" exact component={Home}/>
+                <Route path="/task" exact component={Task}/>
+            </Switch>
+        </BrowserRouter>
+    )
+}
