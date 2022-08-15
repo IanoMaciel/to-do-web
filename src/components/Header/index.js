cb6dcd7f91069839529ebf5880150807708df007
IanoMaciel <ianomaciel6385@gmail.com>
2022-08-15 16:02:32 -0400

    Refactoring: components Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,13 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import {Link} from 'react-router-dom'
 import * as S from './styles'
 
 import logo from '../../assets/logo.png'
 import bell from '../../assets/bell.png'
 
+import api from '../../services/api'
 
 
-function Header({ lateCount, clickNotification }) {
+
+function Header({ clickNotification }) {
+  const [lateCount, setLateCount] = useState();
+
+  async function lateVerify() {
+    await api.get(`/task/filter/late/11:11:11:11:11:11`)
+    .then(response => {
+      setLateCount(response.data.length);
+    })
+  }
+
+  useEffect(() => {
+    lateVerify()
+  },[])
+
   return (
     <S.Container>
       <S.LeftSide>
@@ -19,12 +34,17 @@ function Header({ lateCount, clickNotification }) {
         <span className='divider'/>
         <Link to="/task">NOVA TAREFA</Link>
         <span className='divider'/>
-        <a href='#'>SINCRONIZAR CELULAR</a>
-        <span className='divider'/>
-        <button type="button" onClick={clickNotification} id="notification">
-          <img src={bell} alt="Notificação"/>
-          <span>{ lateCount }</span>
-        </button>
+        <Link to="qrcode">SINCRONIZAR CELULAR</Link>
+        {
+          lateCount &&
+          <>
+            <span className='divider'/>
+            <button type="button" onClick={clickNotification} id="notification">
+              <img src={bell} alt="Notificação"/>
+              <span>{ lateCount }</span>
+            </button>
+          </>
+        }
       </S.RigthSide>
     </S.Container>
   );
